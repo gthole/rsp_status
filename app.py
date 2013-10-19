8f13777e9b40947cb413ffe6bf2164cf44c4b188
Greg Thole <gthole@gmail.com>
2013-10-19 11:29:57 -0400

    App api

--- app.py
+++ app.py
@@ -1,21 +1,54 @@
-from jinja2 import Environment, FileSystemLoader
+from backends import mongo
 import os
+import json
+from urlparse import parse_qs
+from datetime import datetime, timedelta
+import logging
+import iso8601
 
-TEMPLATE_ENV = Environment(loader=FileSystemLoader('templates/'))
-TEMPLATE = TEMPLATE_ENV.get_template('index.html')
+ENDPOINTS = ['/temp/', '/motion/', '/flood/']
 
 
 def application(environ, start_response):
-    data = TEMPLATE.render(message="Hello World!")
+    if environ['REQUEST_METHOD'] != 'GET':
+        status = '405'
+        data = {'message': 'Only GET requests supported'}
+    elif environ['PATH_INFO'] in ENDPOINTS:
+        try:
+            status = '200'
+            data = make_response(environ)
+        except iso8601.ParseError:
+            status = '400'
+            data = {'message': 'Malformed parameter'}
+        except:
+            logging.exception("Caught exception:\n\n%s" % environ)
+            status = '500'
+            data = {'message': 'Server error'}
+    else:
+        status = '404'
+        data = {'message': 'Resource not found'}
 
+    content = json.dumps(data)
+    headers = [
+        ("Content-Type", "application/json"),
+        ("Content-Length", str(len(content)))
+    ]
     start_response(
-        "200 OK",
-        [
-            ("Content-Type", "text/html"),
-            ("Content-Length", str(len(data)))
-        ]
+        status,
+        headers
     )
-    return [data]
+    return [content]
+
+
+def make_response(environ):
+    collection = environ['PATH_INFO'].strip('/')
+    params = parse_qs(environ['QUERY_STRING'])
+    if params.get('since'):
+        since = iso8601.parse_date(params['since'][0])
+    else:
+        since = datetime.now() - timedelta(days=7)
+
+    return mongo.get_since(collection, since)
 
 
 if __name__ == '__main__':
--- backends/mongo.py
+++ backends/mongo.py
@@ -1,5 +1,9 @@
 from pymongo import MongoClient
 from config import settings
+from datetime import datetime, timedelta
+import glob
+import json
+import os
 
 CLIENT = MongoClient(settings.DB_URI)
 DB = CLIENT.get_default_database()
@@ -12,9 +16,31 @@ def put(collection_name, data):
 
 def get_since(collection_name, since_date):
     collection = DB[collection_name]
-    return collection.find({"timestamp": {"$gte": since_date}})
+    cursor = collection.find({"time": {"$gte": since_date}})
+    return [
+        {
+            'time': r['time'].isoformat(),
+            'val': r['val']
+        } for r in cursor
+    ]
 
 
 def get_last(collection_name):
     collection = DB[collection_name]
     return collection.find_one()
+
+
+def load_fixtures():
+    """
+    Loads some fake data points into mongo for local development
+    """
+    fixtures = glob.glob(os.path.join('tests', 'fixtures', '*.json'))
+    now = datetime.now()
+    for fixture in fixtures:
+        name = os.path.basename(fixture)[:-5]
+        with open(fixture, 'r') as file_:
+            data = json.loads(file_.read())
+        for index, point in enumerate(data):
+            point['time'] = now - timedelta(hours=index)
+            put(name, point)
+        print 'Loaded %d data points into %s' % (len(data), name)
