a81e024048713c7bfe9b2203d42cced7d388c2de
Greg Thole <gthole@gmail.com>
2013-11-08 18:42:24 -0500

    AJAX urls for mongorest API

--- static/js/app.js
+++ static/js/app.js
@@ -1,50 +1,52 @@
 $(document).ready(function() {
 
-
-  $.getJSON('http://10.0.1.21:5000/temp/',
+  $.getJSON('/api/v1/temp/?limit=1&_order_by=-time',
     function(response) {
-      var latest = response[response.length - 1].val;
+      var latest = response['data'][0].val;
       $("#temp").html(latest.toFixed(1) + '°');
   });
 
-  $.getJSON('http://10.0.1.21:5000/motion/',
+  $.getJSON('/api/v1/motion/?limit=1&_order_by=-time',
     function(response) {
       dateFormat = d3.time.format('%a %b %d, %Y');
       timeFormat = d3.time.format('%I:%M %p');
-      motion = new Date(response[response.length - 1].time);
+      motion = new Date(response['data'][0].time);
       $("#motion").html(dateFormat(motion) + '<br />' + timeFormat(motion));
   });
-
-  $.getJSON('http://10.0.1.21:5000/flood/',
+  
+  var d = new Date();
+  d.setDate(d.getDate() - 1);
+  $.getJSON('/api/v1/flood/?limit=1&_order_by=-time&time__gt=' + d.toISOString(),
     function(response) {
-      if (response.length) {
+      if (response['data'].length) {
         $("#flood").html('<i class="fa fa-warning fa-5x text-warning"></i>');
       } else {
         $("#flood").html('<i class="fa fa-check-square fa-5x text-success"></i>');
       }
   });
 
+  // Only show graphs on large-width screens
   if (window.screen.width > 1000) {
     var d = new Date();
     d.setDate(d.getDate() - 7);
     $.getJSON(
-      /* '/temp/?since=' + d.toISOString() */
-      'http://10.0.1.21:5000/temp/',
+      '/api/v1/temp/?time__gt=' + d.toISOString(),
       function(response) {
         datePlot(response, '#temp-graph');
       });
     $.getJSON(
-      /* '/motion/?since=' + d.toISOString() */
-      'http://10.0.1.21:5000/motion/',
+      '/api/v1/motion/?time_gt=' + d.toISOString(),
       function(response) {
         densityPlot(response, '#motion-graph');
       });
   }
 });
 
+/*
 function renderTemplate(name, selector, context) {
   $.get("templates/" + name + ".html", function(view) {
     $.Mustache.add('template', view);
     $(selector).mustache('template', context);
   });
 }
+*/
